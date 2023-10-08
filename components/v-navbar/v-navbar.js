554747b7764ccdf66aa5afd10786339f0802330f
N0M4D <victor.develops@gmail.com>
2023-10-08 22:01:16 +0200

    <> custom navbar - search bar styles on going <>

--- components/v-navbar/v-navbar.js
+++ components/v-navbar/v-navbar.js
@@ -7,34 +7,42 @@ export class VNavbar extends HTMLElement {
     // Create a shadow DOM for the component
     this.attachShadow({ mode: "open" });
     const template = document.createElement("template");
-    template.innerHTML = `
-        <link rel="stylesheet" href="./components/${this._id}/${this._id}.css">
-        <nav id='${this._id}'>
+    //add css
+    template.innerHTML = `<link rel="stylesheet" href="./components/${this._id}/${this._id}.css">`;
+    //add html
+    template.innerHTML += `
+    <nav id="${this._id}">
         <div class="container">
-          <a id='menu-button'>
-            <img id='favicon' src="../../assets/icon/favicon.svg" alt="favicon" width="30" height="24">
-          </a>
+            <a id="menu-button">
+                <img id="favicon" src="../../assets/icon/favicon.svg" alt="favicon" width="30" height="24">
+            </a>
+            <div id="search-container">
+                <button id="search-button">S</button>
+                <input type="text" id="search-input" placeholder="Search...">
+            </div>
         </div>
-      </nav>
+    </nav>
       `;
 
     // Clone the template content into the shadow DOM
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     // Wait for the DOM to be fully loaded
-    document.addEventListener("DOMContentLoaded", () => {
-      const favicon = this.shadowRoot.getElementById("favicon");
-      const menuButton = this.shadowRoot.getElementById("menu-button");
+    document.addEventListener("DOMContentLoaded", this.afterDOMLoaded());
+  }
 
-      menuButton.addEventListener("click", () => {
-        // Add the 'rotated' class to trigger the rotation animation
-        favicon.classList.add("rotated");
+  afterDOMLoaded() {
+    const favicon = this.shadowRoot.getElementById("favicon");
+    const menuButton = this.shadowRoot.getElementById("menu-button");
 
-        // Remove the 'rotated' class after the animation completes
-        setTimeout(() => {
-          favicon.classList.remove("rotated");
-        }, 500); // Adjust the time to match the animation duration (0.5s in this case)
-      });
+    menuButton.addEventListener("click", () => {
+      // Add the 'rotated' class to trigger the rotation animation
+      favicon.classList.add("rotated");
+
+      // Remove the 'rotated' class after the animation completes
+      setTimeout(() => {
+        favicon.classList.remove("rotated");
+      }, 500); // Adjust the time to match the animation duration (0.5s in this case)
     });
   }
 }
