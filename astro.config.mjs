fcd2501ff426c8c46332fdb660185a9be8f4d6fb
N0M4D <victor@gg-victor.dev>
2024-03-13 21:55:21 +0100

    blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,27 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
-import remarkGfm from 'remark-gfm'
-import remarkSmartypants from 'remark-smartypants'
-import rehypeExternalLinks from 'rehype-external-links'
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  site: "https://astro-blog-template.netlify.app",
+  integrations: [mdx(), svelte(), tailwind()],
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: "nord",
     },
     remarkPlugins: [remarkGfm, remarkSmartypants],
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
-          target: '_blank',
+          target: "_blank",
         },
       ],
     ],
   },
-})
+});
