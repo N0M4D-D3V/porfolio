749f82ff0f3b5e25a821cd424a816ffb54e3ae44
V1CT0R <victor.gonzalez-gaitero@akka.eu>
2024-07-18 20:59:41 +0200

    <!> services section added but not ended yet

---
+++ src/icons/icon.record.ts
@@ -0,0 +1,39 @@
+import Kotlin from "@/icons/skills/Kotlin.astro";
+import Angular from "@/icons/skills/Angular.astro";
+import Apple from "@/icons/skills/Apple.astro";
+import Capacitor from "@/icons/skills/Capacitor.astro";
+import Docker from "@/icons/skills/Docker.astro";
+import ElectronJs from "@/icons/skills/ElectronJs.astro";
+import Git from "@/icons/skills/Git.astro";
+import HTML from "@/icons/skills/HTML.astro";
+import Ionic from "@/icons/skills/Ionic.astro";
+import Java from "@/icons/skills/Java.astro";
+import Linux from "@/icons/skills/Linux.astro";
+import NestJS from "@/icons/skills/NestJS.astro";
+import Python from "@/icons/skills/Python.astro";
+import Spring from "@/icons/skills/Spring.astro";
+import SQL from "@/icons/skills/SQL.astro";
+import Windows from "@/icons/skills/Windows.astro";
+import Node from "@/icons/skills/Node.astro";
+import CSS from "@/icons/skills/CSS.astro";
+
+export const SKILLS_ICONS: Record<any, any> = {
+  Capacitor,
+  Node,
+  Git,
+  Linux,
+  Apple,
+  Windows,
+  Angular,
+  Ionic,
+  ElectronJs,
+  NestJS,
+  Java,
+  Python,
+  SQL,
+  Kotlin,
+  CSS,
+  HTML,
+  Spring,
+  Docker,
+};
